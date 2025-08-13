917e01aca59018754370618a3179ea1659c44f8b
Steve <229538+seestevecode@users.noreply.github.com>
2025-08-13 20:35:46 +0100

    refactor: separate board functions into Game.Board

--- src/Domain/Constants.elm
+++ src/Domain/Constants.elm
@@ -57,3 +57,49 @@ bonusWeightYes =
 bonusWeightNo : Float
 bonusWeightNo =
     80.0
+
+
+tokenDetails :
+    Token
+    ->
+        { image : String
+        , name : String
+        , baseScore : Maybe Int
+        }
+tokenDetails token =
+    case token of
+        Standard1 ->
+            { image = "high-grass", name = "Grass", baseScore = Just 1 }
+
+        Standard2 ->
+            { image = "daisy", name = "Daisy", baseScore = Just 1 }
+
+        Standard3 ->
+            { image = "spoted-flower"
+            , name = "Spotted flower"
+            , baseScore = Just 1
+            }
+
+        Growing1 _ ->
+            { image = "sesame", name = "Seeds", baseScore = Nothing }
+
+        Growing2 _ ->
+            { image = "bud", name = "Bud", baseScore = Nothing }
+
+        Growing3 _ ->
+            { image = "bulb", name = "Bulb", baseScore = Nothing }
+
+        Grown1 ->
+            { image = "sunflower", name = "Sunflower", baseScore = Just 3 }
+
+        Grown2 ->
+            { image = "rose", name = "Rose", baseScore = Just 6 }
+
+        Grown3 ->
+            { image = "viola", name = "Viola", baseScore = Just 15 }
+
+        Disappearing _ ->
+            { image = "dandelion-flower"
+            , name = "Dandelion"
+            , baseScore = Just 2
+            }
--- src/Main.elm
+++ src/Main.elm
@@ -9,12 +9,11 @@ import Element.Border as Border
 import Element.Events as Events
 import Element.Font as Font
 import Element.Input as Input
+import Game.Board as Board
 import Game.Gen as Gen
 import Html exposing (Html)
 import List.Extra as ListX
 import Random
-import Set exposing (Set)
-import Simplex
 import UI.Colours as Colours
 
 
@@ -90,7 +89,7 @@ initGrid initInt =
 
         initBases =
             List.map2 Tuple.pair Const.coords <|
-                List.map (baseFromCoord initInt) Const.coords
+                List.map (Board.getBaseFromCoord initInt) Const.coords
 
         ( initCoords, coordSeed ) =
             Random.step Gen.initCoordsGenerator initSeed
@@ -113,24 +112,6 @@ initGrid initInt =
         initBases
     , tokenSeed
     )
-
-
-baseFromCoord : Int -> Coord -> Base
-baseFromCoord initInt ( x, y ) =
-    Simplex.noise2d
-        (Simplex.permutationTableFromInt initInt)
-        (toFloat x * Const.terrainScale)
-        (toFloat y * Const.terrainScale)
-        |> (\float ->
-                if float < -(1 / 3) then
-                    Base1
-
-                else if float < 1 / 3 then
-                    Base2
-
-                else
-                    Base3
-           )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -168,7 +149,7 @@ modelAfterPlaceToken oldModel coord =
                 oldModel.currentSeed
 
         newGrid =
-            placeTokenOnGrid oldModel.board.grid
+            Board.placeTokenOnGrid oldModel.board.grid
                 (Tuple.first oldModel.board.queue)
                 coord
 
@@ -187,7 +168,7 @@ modelAfterPlaceToken oldModel coord =
             { grid = newGrid, queue = newQueue }
     in
     { oldModel
-        | gameState = gameStateAfterPlacement newBoard
+        | gameState = Board.advanceGameStateAfterPlacement newBoard
         , board = newBoard
         , currentSeed =
             case oldModel.board.queue of
@@ -202,30 +183,14 @@ modelAfterPlaceToken oldModel coord =
     }
 
 
-gameStateAfterPlacement : Board -> GameState
-gameStateAfterPlacement board =
-    let
-        emptyCells =
-            List.filter (\cell -> cell.content == Nothing) board.grid
-
-        boardFull =
-            List.length emptyCells == 0
-    in
-    if boardFull && Tuple.first board.queue /= Harvester then
-        GameOver
-
-    else
-        Playing
-
-
 modelAfterHarvest : Model -> Coord -> Model
 modelAfterHarvest oldModel coord =
     let
         newBoard =
             { grid =
-                clearHarvest oldModel.board.grid
-                    (harvestFrom oldModel.board.grid coord |> List.map .coord)
-                    |> List.map growCell
+                Board.clearHarvest oldModel.board.grid
+                    (Board.harvestFrom oldModel.board.grid coord |> List.map .coord)
+                    |> List.map Board.growCell
             , queue =
                 case oldModel.board.queue of
                     ( Harvester, x :: xs ) ->
@@ -239,241 +204,12 @@ modelAfterHarvest oldModel coord =
         | board = newBoard
         , score =
             oldModel.score
-                + (harvestFrom oldModel.board.grid coord |> scoreHarvest)
+                + (Board.harvestFrom oldModel.board.grid coord |> Board.scoreHarvest)
         , undoAllowed = True
         , undoSeed = oldModel.currentSeed
         , undoBoard = oldModel.board
         , undoScore = oldModel.score
     }
-
-
-growCell : Cell -> Cell
-growCell cell =
-    { cell
-        | content =
-            case cell.content of
-                Just (Plant token bonus) ->
-                    if token == Disappearing 1 then
-                        Nothing
-
-                    else
-                        Just <| Plant (growToken token) bonus
-
-                _ ->
-                    Nothing
-    }
-
-
-growToken : Token -> Token
-growToken token =
-    case token of
-        Growing1 untilGrowth ->
-            if untilGrowth > 1 then
-                Growing1 (untilGrowth - 1)
-
-            else
-                Grown1
-
-        Growing2 untilGrowth ->
-            if untilGrowth > 1 then
-                Growing2 (untilGrowth - 1)
-
-            else
-                Grown2
-
-        Growing3 untilGrowth ->
-            if untilGrowth > 1 then
-                Growing3 (untilGrowth - 1)
-
-            else
-                Grown3
-
-        Disappearing untilGone ->
-            Disappearing (untilGone - 1)
-
-        _ ->
-            token
-
-
-clearHarvest : Grid -> List Coord -> Grid
-clearHarvest grid harvestCoords =
-    List.map
-        (\cell ->
-            if List.member cell.coord harvestCoords then
-                { cell | base = nextBase cell.base, content = Nothing }
-
-            else
-                cell
-        )
-        grid
-
-
-scoreHarvest : List Cell -> Int
-scoreHarvest cells =
-    let
-        bonusCount =
-            List.map .content cells
-                |> List.filterMap identity
-                |> List.map getBonusFromContent
-                |> List.filterMap identity
-                |> List.filter (\bonus -> bonus == Bonus)
-                |> List.length
-
-        harvestToken =
-            cells
-                |> List.head
-                |> Maybe.withDefault
-                    { base = Base1, coord = ( 0, 0 ), content = Nothing }
-                |> getTokenFromCell
-                |> Maybe.withDefault Standard1
-
-        tokenMultiplier =
-            tokenDetails harvestToken |> .baseScore |> Maybe.withDefault 0
-    in
-    List.indexedMap (\index _ -> (index + bonusCount + 1) * tokenMultiplier)
-        cells
-        |> List.sum
-
-
-getTokenFromCell : Cell -> Maybe Token
-getTokenFromCell cell =
-    case cell.content of
-        Just (Plant token _) ->
-            Just token
-
-        _ ->
-            Nothing
-
-
-getBonusFromContent : Content -> Maybe Bonus
-getBonusFromContent content =
-    case content of
-        Plant _ bonus ->
-            Just bonus
-
-        Harvester ->
-            Nothing
-
-
-applyUntil : (a -> Bool) -> (a -> a) -> a -> a
-applyUntil pred step a =
-    if pred a then
-        a
-
-    else
-        applyUntil pred step (step a)
-
-
-harvestFrom : Grid -> Coord -> List Cell
-harvestFrom grid initCoord =
-    { toCheck = Set.singleton initCoord
-    , toHarvest = Set.singleton initCoord
-    , checked = Set.empty
-    }
-        |> applyUntil (\acc -> acc.toCheck == Set.empty) (harvestStep grid)
-        |> .toHarvest
-        |> Set.toList
-        |> List.map (getCell grid)
-        |> List.filterMap identity
-
-
-harvestStep : Grid -> HarvestInfo -> HarvestInfo
-harvestStep grid harvestInfo =
-    let
-        checkCoord =
-            Set.toList harvestInfo.toCheck
-                |> List.head
-                |> Maybe.withDefault ( -1, -1 )
-
-        checkedCoordSet =
-            Set.singleton checkCoord
-    in
-    case matchedNeighbours grid checkCoord of
-        [] ->
-            { harvestInfo
-                | toCheck = Set.diff harvestInfo.toCheck checkedCoordSet
-                , checked = Set.union harvestInfo.checked checkedCoordSet
-            }
-
-        matches ->
-            { harvestInfo
-                | toCheck =
-                    Set.diff harvestInfo.toCheck checkedCoordSet
-                        |> Set.union
-                            (Set.diff
-                                (Set.fromList matches)
-                                harvestInfo.checked
-                            )
-                , toHarvest =
-                    Set.union harvestInfo.toHarvest (Set.fromList matches)
-                , checked =
-                    Set.union harvestInfo.checked checkedCoordSet
-            }
-
-
-matchedNeighbours : Grid -> Coord -> List Coord
-matchedNeighbours grid coord =
-    List.map (neighbour grid coord) [ Above, Right, Below, Left ]
-        |> List.filterMap identity
-        |> List.map .coord
-        |> List.filter (harvestMatch grid coord)
-
-
-harvestMatch : Grid -> Coord -> Coord -> Bool
-harvestMatch grid coordA coordB =
-    let
-        cellA =
-            getCell grid coordA
-
-        cellB =
-            getCell grid coordB
-    in
-    case ( cellA, cellB ) of
-        ( Just a, Just b ) ->
-            harvestMatchCells a b
-
-        _ ->
-            False
-
-
-harvestMatchCells : Cell -> Cell -> Bool
-harvestMatchCells cellA cellB =
-    let
-        baseMatch =
-            cellA.base == cellB.base
-
-        contentMatch =
-            case ( cellA.content, cellB.content ) of
-                ( Just (Plant tokenA _), Just (Plant tokenB _) ) ->
-                    harvestMatchTokens tokenA tokenB
-
-                _ ->
-                    False
-    in
-    baseMatch && contentMatch
-
-
-harvestMatchTokens : Token -> Token -> Bool
-harvestMatchTokens tokenA tokenB =
-    case ( tokenA, tokenB ) of
-        ( Disappearing _, Disappearing _ ) ->
-            True
-
-        ( _, _ ) ->
-            tokenA == tokenB
-
-
-placeTokenOnGrid : Grid -> Content -> Coord -> Grid
-placeTokenOnGrid oldGrid newContent targetCoord =
-    List.map
-        (\cell ->
-            if cell.coord == targetCoord then
-                { cell | content = Just newContent }
-
-            else
-                cell
-        )
-        oldGrid
 
 
 view : Model -> Html Msg
@@ -617,16 +353,11 @@ viewGrid : Board -> Ui.Element Msg
 viewGrid board =
     let
         viewRow y =
-            getRow y board.grid
+            Board.getRow y board.grid
                 |> List.map (viewCell board)
                 |> Ui.row [ Ui.spacing 0 ]
     in
     Const.axis |> List.map viewRow |> Ui.column [ Ui.spacing 0 ]
-
-
-getRow : Int -> Grid -> List Cell
-getRow row grid =
-    List.filter (\cell -> Tuple.first cell.coord == row) grid
 
 
 viewCell : Board -> Cell -> Ui.Element Msg
@@ -665,11 +396,11 @@ cellColourAtts grid cell =
                 altBaseColour
 
         borderBase =
-            if neighbourBase grid cell.coord Below == Just cell.base then
+            if Board.neighbourBase grid cell.coord Below == Just cell.base then
                 cell.base
 
             else
-                nextBase cell.base
+                Board.nextBase cell.base
     in
     [ Background.color <| colourFn cell.base
     , Border.color <| colourFn borderBase
@@ -703,7 +434,7 @@ cellOnClickAtts ( head, _ ) cell =
 
 cellVerticalAtts : Grid -> Cell -> List (Ui.Attribute Msg)
 cellVerticalAtts grid cell =
-    case ( flushTo grid cell Above, flushTo grid cell Below ) of
+    case ( Board.flushTo grid cell Above, Board.flushTo grid cell Below ) of
         ( True, True ) ->
             [ Ui.height <| Ui.px 100 ]
 
@@ -719,7 +450,7 @@ cellVerticalAtts grid cell =
 
 cellHorizontalAtts : Grid -> Cell -> List (Ui.Attribute Msg)
 cellHorizontalAtts grid cell =
-    case ( flushTo grid cell Right, flushTo grid cell Left ) of
+    case ( Board.flushTo grid cell Right, Board.flushTo grid cell Left ) of
         ( True, True ) ->
             [ Ui.width <| Ui.px 100 ]
 
@@ -733,12 +464,6 @@ cellHorizontalAtts grid cell =
             [ Ui.width <| Ui.px 96, Ui.centerX ]
 
 
-flushTo : Grid -> Cell -> Direction -> Bool
-flushTo grid cell direction =
-    (neighbourBase grid cell.coord direction == Just cell.base)
-        || (neighbourBase grid cell.coord direction == Nothing)
-
-
 cellCornerAtts : Grid -> Cell -> Ui.Attribute Msg
 cellCornerAtts grid cell =
     let
@@ -750,51 +475,11 @@ cellCornerAtts grid cell =
                 8
     in
     Border.roundEach
-        { topRight = cornerBaseMatch grid cell [ Above, Right ] |> round
-        , bottomRight = cornerBaseMatch grid cell [ Right, Below ] |> round
-        , bottomLeft = cornerBaseMatch grid cell [ Below, Left ] |> round
-        , topLeft = cornerBaseMatch grid cell [ Left, Above ] |> round
+        { topRight = Board.cornerBaseMatch grid cell [ Above, Right ] |> round
+        , bottomRight = Board.cornerBaseMatch grid cell [ Right, Below ] |> round
+        , bottomLeft = Board.cornerBaseMatch grid cell [ Below, Left ] |> round
+        , topLeft = Board.cornerBaseMatch grid cell [ Left, Above ] |> round
         }
-
-
-cornerBaseMatch : Grid -> Cell -> List Direction -> Bool
-cornerBaseMatch grid cell directions =
-    let
-        neighbourBases =
-            List.map (neighbourBase grid cell.coord) directions
-                |> List.filterMap identity
-    in
-    List.member cell.base neighbourBases
-
-
-neighbourBase : Grid -> Coord -> Direction -> Maybe Base
-neighbourBase grid coord direction =
-    Maybe.map .base <| neighbour grid coord direction
-
-
-neighbour : Grid -> Coord -> Direction -> Maybe Cell
-neighbour grid ( x, y ) direction =
-    let
-        neighbourCoord =
-            case direction of
-                Above ->
-                    ( x - 1, y )
-
-                Right ->
-                    ( x, y + 1 )
-
-                Below ->
-                    ( x + 1, y )
-
-                Left ->
-                    ( x, y - 1 )
-    in
-    getCell grid neighbourCoord
-
-
-getCell : Grid -> Coord -> Maybe Cell
-getCell grid coord =
-    List.filter (\cell -> cell.coord == coord) grid |> List.head
 
 
 baseColour : Base -> Ui.Color
@@ -821,19 +506,6 @@ altBaseColour base =
 
         Base3 ->
             Colours.altBgBase3
-
-
-nextBase : Base -> Base
-nextBase base =
-    case base of
-        Base1 ->
-            Base2
-
-        Base2 ->
-            Base3
-
-        Base3 ->
-            Base1
 
 
 viewContent : Content -> Ui.Element Msg
@@ -898,7 +570,7 @@ viewTokenCount token =
         innerAtts =
             [ Ui.centerX, Ui.centerY, Font.size 16, Font.color fontColour ]
     in
-    case getTokenCount token of
+    case Board.getTokenCount token of
         Just count ->
             Ui.el outerAtts <|
                 Ui.el innerAtts <|
@@ -909,81 +581,16 @@ viewTokenCount token =
             Ui.none
 
 
-getTokenCount : Token -> Maybe Int
-getTokenCount token =
-    case token of
-        Growing1 count ->
-            Just count
-
-        Growing2 count ->
-            Just count
-
-        Growing3 count ->
-            Just count
-
-        Disappearing count ->
-            Just count
-
-        _ ->
-            Nothing
-
-
 tokenImageDetails : Token -> { src : String, description : String }
 tokenImageDetails token =
     let
         imageName =
-            tokenDetails token |> .image
+            Const.tokenDetails token |> .image
 
         description =
-            tokenDetails token |> .name
+            Const.tokenDetails token |> .name
     in
     { src = "images/" ++ imageName ++ ".png", description = description }
-
-
-tokenDetails :
-    Token
-    ->
-        { image : String
-        , name : String
-        , baseScore : Maybe Int
-        }
-tokenDetails token =
-    case token of
-        Standard1 ->
-            { image = "high-grass", name = "Grass", baseScore = Just 1 }
-
-        Standard2 ->
-            { image = "daisy", name = "Daisy", baseScore = Just 1 }
-
-        Standard3 ->
-            { image = "spoted-flower"
-            , name = "Spotted flower"
-            , baseScore = Just 1
-            }
-
-        Growing1 _ ->
-            { image = "sesame", name = "Seeds", baseScore = Nothing }
-
-        Growing2 _ ->
-            { image = "bud", name = "Bud", baseScore = Nothing }
-
-        Growing3 _ ->
-            { image = "bulb", name = "Bulb", baseScore = Nothing }
-
-        Grown1 ->
-            { image = "sunflower", name = "Sunflower", baseScore = Just 3 }
-
-        Grown2 ->
-            { image = "rose", name = "Rose", baseScore = Just 6 }
-
-        Grown3 ->
-            { image = "viola", name = "Viola", baseScore = Just 15 }
-
-        Disappearing _ ->
-            { image = "dandelion-flower"
-            , name = "Dandelion"
-            , baseScore = Just 2
-            }
 
 
 sharedAttributes : List (Ui.Attribute Msg)
